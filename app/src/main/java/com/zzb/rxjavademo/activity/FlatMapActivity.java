a6468cf73312631273a98d7e378daf7a7ba90dd8
BaronZ <realbo89@outlook.com>
2016-08-10 17:40:17 +0800

    FlatMap

--- app/src/main/java/com/zzb/rxjavademo/activity/FlatMapActivity.java
+++ app/src/main/java/com/zzb/rxjavademo/activity/FlatMapActivity.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import rx.Observable;
 import rx.Observer;
+import rx.functions.Action1;
 import rx.functions.Func1;
 
 public class FlatMapActivity extends BaseActivity {
@@ -19,13 +20,38 @@ public class FlatMapActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flat_map);
         println("flatMap，相当于把Observable<List<String>里面的数据遍历出来");
+        println("flatMap，会遍历每一个Observable");
         println();
         approach0();
         println();
         approach1();
-
+        println();
+        approach2();
     }
-    private void approach1(){
+
+    private void approach2() {
+        println("把下面3个字符里，去掉/, 再输出数字 \n \"1/5/8\", \"1/9/11/58/16/\", \"9/15/56/49/21\"");
+
+        Observable.just("1/5/8", "1/9/11/58/16/", "9/15/56/49/21").flatMap(new Func1<String, Observable<String>>() {
+            @Override
+            public Observable<String> call(String s) {
+                println("flatMap.call(String s):" + s);
+                return spiltSlash(s);
+            }
+        }).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                println("subscribe.call(String s):" + s);
+            }
+        });
+    }
+
+    private Observable<String> spiltSlash(String s) {
+        return Observable.from(s.split("/"));
+//        return Observable.just(s.length() + "");
+    }
+
+    private void approach1() {
         Observable.just(Arrays.asList("A", "B2", "C", "D")).flatMap(new Func1<List<String>, Observable<String>>() {
             @Override
             public Observable<String> call(List<String> strings) {
@@ -56,6 +82,7 @@ public class FlatMapActivity extends BaseActivity {
             }
         });
     }
+
     private void approach0() {
         Observable.just(Arrays.asList("A", "B2", "C", "D")).flatMap(new Func1<List<String>, Observable<String>>() {
             @Override
